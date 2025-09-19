cd3fd8649f4a51731901f3e73acfeacd972ff0de
Stefan Wimmer <stefan.wimmer@trubinekreuzberg.com>
2025-09-19 11:33:43 +0200

    added json format to response from smoothie froot API added dataframe to display json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json( ))
+sf_df = st.dataframe(smoothiefroot_response.json( ), use_container_width=True)
