12fc8a7d0251a5059b8c1d4c5503fd5ce82283c5
Stefan Wimmer <stefan.wimmer@trubinekreuzberg.com>
2025-09-19 11:28:50 +0200

    added request package to requirements.txt and used it in streamlit_app.py to call a public API added requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+import requests
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -45,3 +47,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"""Your Smoothie is ordered, {name_on_order}!""", icon="✅")
+
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
