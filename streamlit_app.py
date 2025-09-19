c6c5840df892bdc168a680f2f7423285e82f2d5b
Stefan Wimmer <stefan.wimmer@trubinekreuzberg.com>
2025-09-19 12:02:48 +0200

    added pandas modified dataframe display and added nutrition info from smoothie froot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -17,7 +17,11 @@ st.write("The name on Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
